73f7de297158279f7a29030d9caf0ffa24fec4e7
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-16 16:39:25 -0400

    Successful queries for supplier phone

--- queries.sql
+++ queries.sql
@@ -1,17 +1,15 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM Customers WHERE postalcode = 1010;
 
-CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
-12	Cactus Comidas para llevar	Patricio Simpson	Cerrito 333	Buenos Aires	1010	Argentina
-54	Océano Atlántico Ltda.	Yvonne Moncada	Ing. Gustavo Moncada 8585 Piso 20-A	Buenos Aires	1010	Argentina
-64	Rancho grande	Sergio Gutiérrez	Av. del Libertador 900	Buenos Aires	1010	Argentina
 -- Find the phone number for the supplier with the id 11
+SELECT phone FROM [Suppliers] where supplierid = 11;
 
 -- List first 10 orders placed, sorted descending by the order date
 
 -- Find all customers that live in London, Madrid, or Brazil
-
+SELECT * FROM Customers WHERE city = 'London' or city = 'Madrid' or city = 'Brazil';
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
